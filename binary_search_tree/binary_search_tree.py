0d3e59f0e440a618c9134e195a38a43a1c6480b1
isaac stott <isaac.cloyd@gmail.com>
2020-07-16 16:57:28 -0600

    mvp for dS 4.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -26,7 +26,7 @@ class BSTNode:
             if self.right is None:
                 self.right = BSTNode(value)
             else:
-                self.right.insert(value)        
+                self.right.insert(value)     
 
     # Return True if the tree contains the value
     # False if it does not
@@ -60,27 +60,121 @@ class BSTNode:
         if self.left:
             self.left.for_each(fn)
 
+    def iterative_depth_first_for_each(self, fn):
+        # DFT: LIFO 
+        # we'll use a stack 
+        stack = []
+        stack.append(self)
+
+        # so long as our stack has nodes in it
+        # there's more nodes to traverse
+        while len(stack) > 0:
+            # pop the top node from the stack 
+            current = stack.pop()
+
+            # add the current node's right child first 
+            if current.right:
+                stack.append(current.right)
+
+            # add the current node's left child 
+            if current.left:
+                stack.append(current.left)
+
+            # call the anonymous function 
+            fn(current.value)
+
+    # def iterative_breadth_first_for_each(self, fn):
+    #     from collections import deque
+
+    #     # BFT: FIFO 
+    #     # we'll use a queue to facilitate the ordering 
+    #     queue = deque()
+    #     queue.append(self)
+
+    #     # continue to traverse so long as there are nodes in the queue
+    #     while len(queue) > 0:
+    #         current = queue.popleft()
+
+    #         if current.left:
+    #             queue.append(current.left)
+
+    #         if current.right:
+    #             queue.append(current.right)
+
+    #         fn(current.value)
+
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            node.left.in_order_print(node.left)
+        print(node.value)
+        if node.right:
+            node.right.in_order_print(node.right)
+
+
+        # newList = []
+        # newList.append(self.value)
+        
+        
+        # if self.left :
+        #     self.left.in_order_print(node)
+
+        # if self.right:
+        #     self.right.in_order_print(node)
+
+        # flatList = [val for sublist in newList for val in sublist]
+
+        # print(f"{flatList}")
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        from collections import deque
+
+        queue = deque()
+        queue.append(node)
+
+        # continue to traverse so long as there are nodes in the queue
+        while len(queue) > 0:
+            current = queue.popleft()
+
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
+
+            print(current.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        stack.append(node)
+
+        while len(stack) > 0:
+            current = stack.pop()
+
+            if current.right:
+                stack.append(current.right)
+
+            if current.left:
+                stack.append(current.left)
+
+            print(current.value)
+
+        
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
+    
     def pre_order_dft(self, node):
         pass
 
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -128,6 +128,7 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
+        
         self.delete(node)
         self.add_to_head(node.value)
         
